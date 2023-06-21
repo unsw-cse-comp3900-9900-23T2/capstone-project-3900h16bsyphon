695b0c1b254c03a3554783965db8fdba67564868
Peter Derias <peter_derias@hotmail.com>
2023-06-21 16:13:15 +1000

    add local running script

---
+++ run_app.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+exit_script() {
+    # kill everything on 8000 and 3000
+    PORT_NUMBER=8000
+    lsof -it tcp:${PORT_NUMBER} | xargs kill
+    PORT_NUMBER=3000
+    lsof -it tcp:${PORT_NUMBER} | xargs kill
+    trap - SIGINT SIGTERM # clear the trap
+}
+
+trap exit_script SIGINT SIGTERM
+
+# a way to run the app using docker only for the database
+# run only DB in docker
+set -a # automatically export all variables
+DATABASE_HOST=localhost
+source backend/.env
+set +a # stop exporting variables
+
+docker compose up -d database
+
+cd backend
+
+# must have cargo, and cargo-watch
+cargo watch -x run &
+
+cd ../frontend
+set -a # automatically export all variables
+source .env
+set +a # stop exporting variables
+# must have run npm install
+npm run dev
