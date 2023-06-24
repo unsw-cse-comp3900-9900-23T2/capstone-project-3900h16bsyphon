4c31a397066cc8833fdb19d8e1007f2ea29fb8ac
Peter Derias <peter_derias@hotmail.com>
2023-06-24 18:00:30 +1000

    format

--- frontend/components/Button/index.tsx
+++ frontend/components/Button/index.tsx
@@ -2,9 +2,9 @@ import MuiButton, { ButtonProps } from '@mui/material/Button';
 import styles from './Button.module.css';
 
 export default function Button({ children, ...props }: { children: any } & ButtonProps) {
-    return (
-        <MuiButton className={styles.button} {...props}>
-            {children}
-        </MuiButton>
-    );
+  return (
+    <MuiButton className={styles.button} {...props}>
+      {children}
+    </MuiButton>
+  );
 }
--- frontend/components/Logo/index.tsx
+++ frontend/components/Logo/index.tsx
@@ -2,5 +2,5 @@ import styles from './Logo.module.css';
 import Link from 'next/link';
 
 export default function Logo() {
-    return <div className={styles.logo}><Link href={'/'}>Syphon</Link></div>;
+  return <div className={styles.logo}><Link href={'/'}>Syphon</Link></div>;
 }
