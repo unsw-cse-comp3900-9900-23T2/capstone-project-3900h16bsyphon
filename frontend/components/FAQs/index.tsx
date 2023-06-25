acc7d234e72f75344f7d42db77bc59f14d94a2d1
hdphuong <vuonghdphuong@gmail.com>
2023-06-26 08:58:42 +1000

    page size

--- frontend/components/FAQs/index.tsx
+++ frontend/components/FAQs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import style from './FAQs.module.css';
 import { GridRowSpacingParams } from '@mui/x-data-grid';
@@ -11,10 +11,10 @@ const FAQs = () => {
             'Questions': 'What do you call a fake noodle?',
             'Answer': 'An Impasta!' },
             { 'id': 3,
-            'Questions': ' Why did the math book look so sad?',
+            'Questions': "Why did the math book look so sad?",
             'Answer': "The math book had been feeling down lately, burdened by the weight of numbers and equations that seemed to stretch endlessly across its pages. It had grown tired of being seen as a mere tool for solving problems, longing to be appreciated for the beauty and elegance hidden within its mathematical realm. Each day, as it watched novels being read with delight and poetry being recited with passion, the math book couldn't help but feel left out, confined to a world where its true potential went unnoticed. It yearned for the day when someone would open its cover not out of obligation, but with genuine curiosity and a desire to explore the intricacies of numbers. The math book dreamt of being embraced, not for its ability to calculate, but for its power to unlock the secrets of the universe and inspire minds. So, as it sat on the shelf, a tinge of sadness permeated its pages, silently hoping that one day, someone would see beyond its numerical facade and discover the wonders that lay within."
             }];
-    const getRowSpacing = React.useCallback((params: GridRowSpacingParams) => {
+    const getRowSpacing = useCallback((params: GridRowSpacingParams) => {
         return {
             top: params.isFirstVisible ? 0 : 5,
             bottom: params.isLastVisible ? 0 : 5,
@@ -30,8 +30,8 @@ const FAQs = () => {
                 rows={data}
                 className={style.grid}
                 getRowHeight={() => 'auto'} 
-                autoHeight
-                autoPageSize
+                autoHeight={true}
+                pageSizeOptions={[5, 10, 20]}
                 getRowSpacing={getRowSpacing}
                 sx={{
                     border: 0.5,
