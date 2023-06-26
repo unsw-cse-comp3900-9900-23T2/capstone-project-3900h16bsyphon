85192e2f8868467aa4132ba12d609cf3d4766bf8
imagine-hussain <hussain.nwz000@gmail.com>
2023-06-26 17:32:55 +1000

    add admn users

--- backend/src/prelude.rs
+++ backend/src/prelude.rs
@@ -1,9 +1,17 @@
 //! This file contains all the prelude imports for the project.
 //! Helps in keeping `main.rs` clean.
 
+use std::thread;
+
+use actix_web::{web::Json, HttpResponse};
+use futures::executor::block_on;
 /// Secret used to hash passwords.
 /// Requires `SECRET` to be set as and environemnt variable or in
 /// a `.env` file in the root of the project, or a parent folder.
+use serde::{Deserialize, Serialize};
+
+use crate::server::auth::{create_user, CreateUserBody};
+
 pub const SECRET: &'static str = dotenv_codegen::dotenv!("SECRET");
 const DEFAULT_PORT: u16 = 8000;
 const DEFAULT_HOST: &'static str = "0.0.0.0";
@@ -19,6 +27,44 @@ pub fn host_port_from_env() -> (String, u16) {
         .unwrap_or(None)
         .unwrap_or(DEFAULT_PORT);
     (host, port)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    pub org_admins: Vec<CreateUserBody>,
+}
+
+pub async fn register_org_admins() {
+    let admin_pass = std::env::var("ADMIN_PASS").expect("ADMIN_PASS not set");
+    let created_res = create_user(Json(CreateUserBody {
+        first_name: "Admin".into(),
+        last_name: "Admin".into(),
+        zid: "z0000000".into(),
+        password: admin_pass,
+    }))
+    .await;
+    match created_res.status() {
+        actix_web::http::StatusCode::OK => log::info!("Created admin user"),
+        actix_web::http::StatusCode::CONFLICT => log::warn!("Admin already exists"),
+        actix_web::http::StatusCode::INTERNAL_SERVER_ERROR => {
+            return log::error!("Internal Error While Creating Admin")
+        }
+        _ => return log::error!("Error when creating admin: {:?}", created_res),
+    }
+    // Add Admin Perms
+    crate::server::auth::make_admin("z0000000").await;
+}
+
+#[cfg(debug_assertions)]
+#[inline(always)]
+pub const fn in_release_build() -> bool {
+    false
+}
+
+#[cfg(not(debug_assertions))]
+#[inline(always)]
+pub const fn in_release_build() -> bool {
+    true
 }
 
 /// Instantiates `env_logger` with the appropriate settings based on the environment.
@@ -49,16 +95,4 @@ pub fn startup_logger() {
     log::info!("Logger set up successfully!");
     log::info!("");
     log::info!("");
-}
-
-#[cfg(debug_assertions)]
-#[inline(always)]
-pub const fn in_release_build() -> bool {
-    false
-}
-
-#[cfg(not(debug_assertions))]
-#[inline(always)]
-pub const fn in_release_build() -> bool {
-    true
 }
