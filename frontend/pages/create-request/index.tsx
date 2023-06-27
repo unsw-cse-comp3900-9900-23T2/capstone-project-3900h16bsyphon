bcd0b9d9de90ed94852bad7bff55acb39f720f75
Aisha Nauman <aishanauman01@gmail.com>
2023-06-27 10:39:38 +1000

    some what done w wordcount stuff

--- frontend/pages/create-request/index.tsx
+++ frontend/pages/create-request/index.tsx
@@ -1,30 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import { Box, Card, CardContent, TextField, Typography, Button } from '@mui/material';
 import styles from './CreateRequest.module.css';
 import { useRouter } from 'next/router';
 
-interface UserProfileCardProps {
-  zid: string;
-  firstName: string;
-  lastName: string;
-  isUserAdmin: boolean;
-  tutor: string[];
-  courseAdmin: string[];
-}
+const MIN_TITLE = 5;
+const MIN_DESCRIPTION = 50;
 
-export default function CreateRequest({
-  zid,
-  firstName,
-  lastName,
-  isUserAdmin,
-  tutor,
-  courseAdmin,
-}: UserProfileCardProps) {
+export default function CreateRequest() {
 
   const router = useRouter();
+  const [title, setTitle] = useState('');
+  const [titleWordCount, setTitleWordCount] = useState(0);
+
 
   const navigateToDashboard = () => {
     router.push('/dashboard');
   };
+
+  useEffect(() => {
+    if (title.trim() === '') {
+      setTitleWordCount(0);
+    } else  {
+      setTitleWordCount(title.trim().split(' ').length);
+    }
+  }, [title]);
 
   return (
     <div className={styles.pageContainer} >
@@ -38,38 +37,55 @@ export default function CreateRequest({
 
           <CardContent className={styles.cardContent}>
             <div>
-              <Typography className={styles.text} variant="subtitle1">
-                Title
-              </Typography>
+              <div className={styles.headingWordCount}>
+                <Typography className={styles.text} variant="subtitle1">
+                  Title
+                </Typography>
+                <Typography className={styles.text} variant="subtitle1">
+                  {(MIN_TITLE - titleWordCount) < 0 ? 0 : MIN_TITLE - titleWordCount} more words required
+                </Typography>
+              </div>
               <TextField
                 className={styles.text}
-                id="outlined-read-only-input"
-                defaultValue={zid}
+                id="outlined-input"
+                value={title}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setTitle(event.target.value);
+                }}
+                placeholder='Give a descriptive overview of the issue'
+                fullWidth
+              />
+            </div>
+
+            <div>
+              <div className={styles.headingWordCount}>
+                <Typography className={styles.text} variant="subtitle1">
+                  Description
+                </Typography>
+                <Typography className={styles.text} variant="subtitle1">
+                  {titleWordCount} words
+                </Typography>
+              </div>
+              <TextField
+                multiline
+                rows={4}
+                className={styles.text}
+                placeholder='Give a detailed description of the issue. Include any error messages and what you have done so far to try and solve this.'
+                id="outlined-input"
                 fullWidth
               />
             </div>
 
             <div>
               <Typography className={styles.text} variant="subtitle1">
-                Description
+                Tags
               </Typography>
               <TextField
                 multiline
+                rows={4}
                 className={styles.text}
-                id="outlined-read-only-input"
-                defaultValue={firstName}
-                fullWidth
-              />
-            </div>
-
-            <div>
-              <Typography className={styles.text} variant="subtitle1">
-                Last Name
-              </Typography>
-              <TextField
-                className={styles.text}
-                id="outlined-read-only-input"
-                defaultValue={lastName}
+                placeholder='Give a detailed description of the issue. Include any error messages and what you have done so far to try and solve this.'
+                id="outlined-input"
                 fullWidth
               />
             </div>
