eae3dd5c80a30094ee1c5e1faba736c6c835a044
Aisha Nauman <aishanauman01@gmail.com>
2023-06-29 11:12:19 +1000

    fixed error messages

--- backend/src/server/user.rs
+++ backend/src/server/user.rs
@@ -1,11 +1,11 @@
 use actix_web::{web::ReqData, HttpResponse};
 use serde::{Deserialize, Serialize};
 
-use crate::{database_utils::db_connection, entities::{self, clusters::Relation}};
+use crate::{database_utils::db_connection, entities::{self}};
 
 use super::auth::TokenClaims;
 use sea_orm::{
-    ColumnTrait, DatabaseConnection, EntityTrait, FromQueryResult, QueryFilter, QuerySelect, Related, JoinType, RelationTrait,
+    ColumnTrait, DatabaseConnection, EntityTrait, FromQueryResult, QueryFilter, QuerySelect, JoinType, RelationTrait,
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
@@ -15,16 +15,18 @@ pub struct UserReturnModel {
     last_name: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
+pub struct UserPermissionCourseCodeModel {
+    course_code: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UserProfileReturnModel {
     zid: i32,
     first_name: String,
     last_name: String,
-    tutor: Vec<String>,
-    course_admin: Vec<String>,
-}
-
-pub struct CourseCodeModel {
-    course_codes: Vec<String>,
+    tutor: Vec<UserPermissionCourseCodeModel>,
+    course_admin: Vec<UserPermissionCourseCodeModel>,
 }
 
 pub async fn get_users(token: ReqData<TokenClaims>) -> HttpResponse {
@@ -64,56 +66,49 @@ pub async fn get_user(token: ReqData<TokenClaims>) -> HttpResponse {
     
     let user_id = token.username;
 
+    // get all courses user tutors 
+    let tutors = entities::tutors::Entity::find()
+        .select_only()
+        .column(entities::course_offerings::Column::CourseCode)
+        .filter(entities::users::Column::Zid.eq(user_id))
+        .join(JoinType::InnerJoin, entities::tutors::Relation::CourseOfferings.def())
+        .into_model::<UserPermissionCourseCodeModel>()
+        .all(db)
+        .await
+        .unwrap();
+
+    // get all courses user admins
+    let admins = entities::tutors::Entity::find()
+        .select_only()
+        .column(entities::course_offerings::Column::CourseCode)
+        .filter(entities::users::Column::Zid.eq(user_id).and(entities::tutors::Column::IsCourseAdmin.eq(true)))
+        .into_model::<UserPermissionCourseCodeModel>()
+        .all(db)
+        .await
+        .unwrap();
+    
+    // get single user from db
     let user = entities::users::Entity::find_by_id(user_id)
         .select_only()
         .column(entities::users::Column::Zid)
         .column(entities::users::Column::FirstName)
         .column(entities::users::Column::LastName)
+        .into_model::<UserReturnModel>()
         .one(db)
         .await
+        .unwrap()
         .unwrap();
 
-    let tutors: Vec<String> = entities::tutors::Entity::find()
-    .select_only()
-    .column(entities::course_offerings::Column::CourseCode)
-    .filter(entities::users::Column::Zid.eq(user_id))
-    .join(JoinType::InnerJoin, entities::tutors::Relation::CourseOfferings.def())
-    .into()
-    .await
-    .unwrap();
+    let user_return = UserProfileReturnModel {
+        zid: user.zid,
+        first_name: user.first_name.clone(),
+        last_name: user.last_name.clone(),
+        tutor: tutors,
+        course_admin: admins,  
+    };
 
-    let admins: Vec<String> = entities::tutors::Entity::find()
-    .select_only()
-    .column(entities::course_offerings::Column::CourseCode)
-    .filter(entities::users::Column::Zid.eq(user_id).and(entities::tutors::Column::IsCourseAdmin.eq(true)))
-    .into()
-    .await
-    .unwrap();
-
-    // get all courses user tutors 
-    // get all courses user admins
-    
-    // get single user from db
-    let user = entities::users::Entity::find_by_id(user_id)
-    .select_only()
-    .column(entities::users::Column::Zid)
-    .column(entities::users::Column::FirstName)
-    .column(entities::users::Column::LastName)
-    .filter(entities::users::Column::IsOrgAdmin.ne(true))
-    .into_model::<UserReturnModel>()
-    .one(db)
-    .await;
-
-
-
-    // return users
-    match user {
-        Ok(user) => HttpResponse::Ok().json(user),
-        Err(e) => {
-            log::warn!("Db broke?: {:?}", e);
-            HttpResponse::InternalServerError().json("Db Broke")
-        }
-    }
+    // return user
+    HttpResponse::Ok().json(user_return)
 }
 
 pub async fn validate_user(
