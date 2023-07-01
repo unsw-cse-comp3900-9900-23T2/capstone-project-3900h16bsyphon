3b1c241d641c2026d4cf8ac1bad090593d9b4333
hdphuong <vuonghdphuong@gmail.com>
2023-07-01 22:27:13 +1000

    add bold/normal text option to tagbox

--- frontend/components/TagBox/index.tsx
+++ frontend/components/TagBox/index.tsx
@@ -5,11 +5,12 @@ type TagBoxProps = {
     text: string,
     backgroundColor: string,
     color: string,
+    bold?: boolean,
 }
 
-const TagBox = ({ text, backgroundColor, color }: TagBoxProps) => {
+const TagBox = ({ text, backgroundColor, color, bold=true }: TagBoxProps) => {
   return <div className={styles.tagContainer} style={{ backgroundColor }} >
-    <Typography className={styles.tagText} style={{ color }} variant='body1'>{text}</Typography>
+    <Typography style={{ color, fontWeight: bold ? 'bold' : 'normal' }} variant='body1'>{text}</Typography>
   </div>;
 };
 
