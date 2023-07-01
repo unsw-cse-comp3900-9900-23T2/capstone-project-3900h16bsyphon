6306827c838f88e86bf1c4ec7ab60b5773ba8fff
imagine-hussain <hussain.nwz000@gmail.com>
2023-07-01 18:19:47 +1000

    small stuff

--- backend/src/utils/db.rs
+++ backend/src/utils/db.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use futures::executor::block_on;
 use log::info;
-use sea_orm::{Database, DatabaseConnection, ConnectOptions};
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 use lazy_static::__Deref;
 
@@ -10,11 +10,11 @@ lazy_static! {
     static ref DB: DatabaseConnection = {
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         info!("database url: {}", database_url);
-        let mut opt = ConnectOptions::new(database_url);
-        opt.max_connections(100).min_connections(5);
-        block_on(async {
-            Database::connect(opt).await.unwrap()
-        })
+        let opt = ConnectOptions::new(database_url)
+            .max_connections(100)
+            .min_connections(5)
+            .to_owned();
+        block_on(async { Database::connect(opt).await.unwrap() })
     };
 }
 
