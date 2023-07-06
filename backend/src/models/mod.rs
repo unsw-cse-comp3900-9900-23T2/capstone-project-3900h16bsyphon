46455e727b26f2b5f05b58325905195bca2d51eb
imagine-hussain <hussain.nwz000@gmail.com>
2023-07-07 01:20:35 +1000

    added error type + 1 impl

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -5,6 +5,8 @@ pub mod queue;
 pub mod request;
 pub mod user;
 
+
+use actix_web::{HttpResponse, HttpResponseBuilder};
 pub use auth::*;
 pub use course::*;
 pub use history::*;
@@ -12,3 +14,52 @@ pub use queue::*;
 pub use queue::*;
 pub use request::*;
 pub use user::*;
+
+pub type SyphonResult<T> = Result<T, SyphonError>;
+
+#[derive(Debug)]
+pub enum SyphonError {
+    Json(serde_json::Value, actix_web::http::StatusCode),
+    DbError(sea_orm::DbErr)
+}
+
+impl std::fmt::Display for SyphonError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SyphonError::Json(val, _) => std::fmt::Display::fmt(val, f),
+            SyphonError::DbError(_) => write!(f, "Internal Db Error")
+        }
+    }
+}
+
+
+impl SyphonError {
+    fn deserialise_body(&self) -> Result<String, serde_json::Error> {
+        match self {
+            SyphonError::Json(body, _) => serde_json::to_string(body),
+            SyphonError::DbError(_) => Ok(String::from("Internal Db Error")),
+        }
+    }
+}
+
+impl actix_web::ResponseError for SyphonError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            SyphonError::Json(_, code) => *code,
+            SyphonError::DbError(_) => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+
+    fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
+        let json_body = self.deserialise_body().unwrap();
+
+        HttpResponseBuilder::new(self.status_code()).json(json_body)
+    }
+}
+
+impl From<sea_orm::DbErr> for SyphonError {
+    fn from(err: sea_orm::DbErr) -> Self {
+        log::error!("Db Error: {}", err);
+        SyphonError::DbError(err)
+    }
+}
--- backend/src/server/auth.rs
+++ backend/src/server/auth.rs
@@ -1,16 +1,8 @@
-use actix_web::{dev::ServiceRequest, web, HttpMessage, HttpResponse, Responder};
-
-use actix_web_httpauth::{
-    extractors::{
-        basic::BasicAuth,
-        bearer::{self, BearerAuth},
-        AuthenticationError,
-    },
-    headers::www_authenticate::bearer::Bearer,
-};
+use actix_web::{http::StatusCode, web, HttpResponse};
+use actix_web_httpauth::extractors::basic::BasicAuth;
 
 use hmac::{Hmac, Mac};
-use jwt::{SignWithKey, VerifyWithKey};
+use jwt::SignWithKey;
 use sea_orm::{ActiveModelTrait, ActiveValue, EntityTrait};
 use serde_json::json;
 use sha2::Sha256;
@@ -18,122 +10,49 @@ use sha2::Sha256;
 use crate::{
     entities::users,
     models::auth::CreateUserBody,
-    models::{auth::TokenClaims, AuthTokenClaims},
+    models::{auth::TokenClaims, SyphonError, SyphonResult},
     utils::auth::hash_pass,
     utils::db::db,
     SECRET,
 };
 
-/// Handler that validates a bearer token. This is used as the source
-/// for our `HttpAuthentication` middleware.
-pub async fn validator(
-    req: ServiceRequest,
-    credentials: BearerAuth,
-) -> Result<ServiceRequest, (actix_web::Error, ServiceRequest)> {
-    let key: hmac::Hmac<Sha256> = Hmac::new_from_slice(SECRET.as_bytes()).unwrap();
-    let token_string = credentials.token();
-
-    // Validate token
-    let claims: Result<TokenClaims, &str> = token_string
-        .verify_with_key(&key)
-        .map_err(|_| "invalid token");
-
-    match claims {
-        Ok(value) => {
-            req.extensions_mut().insert(value);
-            Ok(req)
-        }
-        Err(_) => {
-            let config_data = req
-                .app_data::<bearer::Config>()
-                .cloned()
-                .unwrap_or_default()
-                .scope("");
-            Err((AuthenticationError::from(config_data).into(), req))
-        }
-    }
-}
-
-/// Handler that validates a bearer token. This is used as the source
-/// for our `HttpAuthentication` middleware.
-pub async fn validator_admin(
-    req: ServiceRequest,
-    credentials: BearerAuth,
-) -> Result<ServiceRequest, (actix_web::Error, ServiceRequest)> {
-    let key: hmac::Hmac<Sha256> = Hmac::new_from_slice(SECRET.as_bytes()).unwrap();
-    let token_string = credentials.token();
-    let db = db();
-    // Validate token
-    let claims: Result<TokenClaims, &str> = token_string
-        .verify_with_key(&key)
-        .map_err(|_| "invalid token");
-
-    match claims {
-        Ok(value) => {
-            match users::Entity::find_by_id(value.username)
-                .one(db)
-                .await
-                .map_err(|e| {
-                    log::warn!("DB Broke when finding user ??:\n\t{}", e);
-                    HttpResponse::InternalServerError().body("AHHHH ME BROKEY BAD")
-                })
-                .expect("Db broke")
-                .expect("User exists cos token is valid")
-                .is_org_admin
-            {
-                true => {
-                    req.extensions_mut().insert(AuthTokenClaims::from(value));
-                    Ok(req)
-                }
-                false => Err((auth_err_from_req(&req).into(), req)),
-            }
-        }
-        Err(_) => Err((auth_err_from_req(&req).into(), req)),
-    }
-}
-
-fn auth_err_from_req(req: &ServiceRequest) -> AuthenticationError<Bearer> {
-    AuthenticationError::from(
-        req.app_data::<bearer::Config>()
-            .cloned()
-            .unwrap_or_default()
-            .scope(""),
-    )
-}
-
 /// Hit this endpoint with BasicAuth info to get a BearerAuth token
 /// Use that token in the Authorization header to access other endpoints
-pub async fn auth(credentials: BasicAuth) -> impl Responder {
+pub async fn auth(credentials: BasicAuth) -> SyphonResult<HttpResponse> {
     let pass = credentials.password();
     let zid = match CreateUserBody::verify_zid(credentials.user_id()) {
         Ok(zid) => zid,
-        Err(e) => return HttpResponse::BadRequest().json(json! {{"zid": e}}),
+        Err(e) => {
+            return Err(SyphonError::Json(
+                json!({ "zid": e }),
+                StatusCode::BAD_REQUEST,
+            ))
+        }
     };
 
     let jwt_secret = Hmac::<Sha256>::new_from_slice(SECRET.as_bytes()).unwrap();
     match pass {
-        None => HttpResponse::Unauthorized().json("no password"),
+        None => Err(SyphonError::Json(json!("no_password"), StatusCode::BAD_REQUEST)),
         Some(pass) => {
             // 1. check user in db
             let db = db();
             let db_user = users::Entity::find_by_id(zid).one(db).await.map_err(|e| {
                 log::warn!("DB Brokee when finding user ??:\n\t{}", e);
-                HttpResponse::InternalServerError().json("AHHHH ME BROKEY BAD")
-            });
+                e
+            })?;
             let user: users::Model = match db_user {
-                Err(e) => return e,
-                Ok(None) => {
-                    return HttpResponse::Unauthorized().json(json! {{"zid": "user not found"}})
+                None => {
+                    return Ok(HttpResponse::Unauthorized().json(json! {{"zid": "user not found"}}))
                 }
-                Ok(Some(user)) => user,
+                Some(user) => user,
             };
 
             // Verify Pw Validity
             if let Err(e) = CreateUserBody::verify_password(pass) {
-                return HttpResponse::BadRequest().json(json! {{"password": e}});
+                return Err(SyphonError::Json(json! ({"password": e}), StatusCode::BAD_REQUEST));
             }
             if user.hashed_pw != hash_pass(pass).unwrap() {
-                return HttpResponse::BadRequest().json(json! {{"password": "incorrect password"}});
+                return Err(SyphonError::Json(json! {{"password": "incorrect password"}}, StatusCode::BAD_REQUEST));
             }
 
             // Create Claims Token
@@ -142,7 +61,7 @@ pub async fn auth(credentials: BasicAuth) -> impl Responder {
                 .sign_with_key(&jwt_secret)
                 .expect("Sign is valid");
 
-            HttpResponse::Ok().json(signed_token)
+            Ok(HttpResponse::Ok().json(signed_token))
         }
     }
 }
