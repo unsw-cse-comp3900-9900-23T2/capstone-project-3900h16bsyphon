9072ff1240fb176ba2f8610dfb115e3dfee32300
imagine-hussain <hussain.nwz000@gmail.com>
2023-07-06 21:59:04 +1000

    remove unused From impl

--- backend/src/models/request.rs
+++ backend/src/models/request.rs
@@ -13,21 +13,6 @@ pub struct CreateRequest {
     pub status: Option<Statuses>,
 }
 
-// impl From<CreateRequest> for entities::requests::ActiveModel {
-//     fn from(value: CreateRequest) -> Self {
-//         Self {
-//             request_id: ActiveValue::NotSet,
-//             zid: ActiveValue::Set(value.zid),
-//             queue_id: ActiveValue::Set(value.queue_id),
-//             title: ActiveValue::Set(value.title),
-//             description: ActiveValue::Set(value.description),
-//             order: ActiveValue::Set(value.order),
-//             is_clusterable: ActiveValue::Set(value.is_clusterable),
-//             status: ActiveValue::Set(value.status),
-//         }
-//     }
-// }
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RequestInfoBody {
     pub request_id: i32,
