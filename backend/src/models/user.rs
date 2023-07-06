a9757b857874d8e2c1e90ec8db56e33535616627
imagine-hussain <hussain.nwz000@gmail.com>
2023-07-06 21:18:41 +1000

    columsn

---
+++ backend/src/models/user.rs
@@ -0,0 +1,28 @@
+use sea_orm::FromQueryResult;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
+pub struct UserReturnModel {
+    pub zid: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub is_org_admin: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
+pub struct UserPermissionCourseCodeModel {
+    pub course_code: String,
+    pub course_offering_id: i32,
+    pub title: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserProfileReturnModel {
+    pub zid: i32,
+    pub is_org_admin: bool,
+    pub first_name: String,
+    pub last_name: String,
+    pub tutor: Vec<UserPermissionCourseCodeModel>,
+    pub course_admin: Vec<UserPermissionCourseCodeModel>,
+}
--- backend/src/server/user.rs
+++ backend/src/server/user.rs
@@ -1,38 +1,12 @@
 use actix_web::{web::ReqData, HttpResponse};
-use serde::{Deserialize, Serialize};
 
 use crate::models::auth::TokenClaims;
+use crate::models::user::*;
 use crate::{entities, utils::db::db};
 
 use sea_orm::{
-    ColumnTrait, DatabaseConnection, EntityTrait, FromQueryResult, JoinType, QueryFilter,
-    QuerySelect, RelationTrait,
+    ColumnTrait, DatabaseConnection, EntityTrait, JoinType, QueryFilter, QuerySelect, RelationTrait,
 };
-
-#[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
-pub struct UserReturnModel {
-    zid: i32,
-    first_name: String,
-    last_name: String,
-    is_org_admin: bool,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, FromQueryResult)]
-pub struct UserPermissionCourseCodeModel {
-    course_code: String,
-    course_offering_id: i32,
-    title: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct UserProfileReturnModel {
-    zid: i32,
-    is_org_admin: bool,
-    first_name: String,
-    last_name: String,
-    tutor: Vec<UserPermissionCourseCodeModel>,
-    course_admin: Vec<UserPermissionCourseCodeModel>,
-}
 
 pub async fn get_users(token: ReqData<TokenClaims>) -> HttpResponse {
     let db = db();
@@ -75,9 +49,11 @@ pub async fn get_user(token: ReqData<TokenClaims>) -> HttpResponse {
     // get all courses user tutors
     let tutors = entities::tutors::Entity::find()
         .select_only()
-        .column(entities::course_offerings::Column::CourseCode)
-        .column(entities::course_offerings::Column::CourseOfferingId)
-        .column(entities::course_offerings::Column::Title)
+        .columns([
+            entities::course_offerings::Column::CourseCode,
+            entities::course_offerings::Column::CourseOfferingId,
+            entities::course_offerings::Column::Title,
+        ])
         .filter(entities::tutors::Column::Zid.eq(user_id))
         .join(
             JoinType::InnerJoin,
