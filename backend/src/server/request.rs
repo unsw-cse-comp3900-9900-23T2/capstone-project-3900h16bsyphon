7f70ea9425d8b8da030658cd5085bd3add2707fc
Aisha Nauman <aishanauman01@gmail.com>
2023-07-10 13:52:58 +1000

    SYP-33: Edit request + refactor tagSelection component + refactor create-request screen (#43)
    * working on frontend for create request card
    
    * written BE route for request/edit (without tags)
    
    * done with handling update tag logic - need to connect everything
    
    * adding edit request button to wait screen
    
    * fixing typo
    
    * fixing tagSelection component
    
    * cleaning up
    
    * edit request works!!! - need to integrate with create request
    
    * updating create request screen
    
    * this tagselection shit has eaten me frfr
    
    * getting rid of unused css
    
    * cleaning up
    
    * fixing up tag priority bug
    
    * fixing up layout for wait screen (again cos im stupid)
    
    * another tag selection bug ahahhaa whats new sdlhfjlsdkfjksdf
    
    * fixing styling
    
    * fixing await? in request.rs
    
    * fixing request page css to match wait screen
    
    * cleaning up error handling in request.rs

--- backend/src/server/request.rs
+++ backend/src/server/request.rs
@@ -1,7 +1,9 @@
 use actix_web::http::StatusCode;
 use actix_web::web::{self, ReqData};
 use actix_web::HttpResponse;
+use serde_json::json;
 
+use crate::models::request::EditRequestBody;
 use crate::{entities, models, utils::db::db};
 use models::request::{AllRequestsForQueueBody, RequestInfoBody};
 
@@ -60,6 +62,71 @@ pub async fn create_request(
     Ok(HttpResponse::Ok().json(CreateRequestResponse {
         request_id: insertion.request_id,
     }))
+}
+
+
+pub async fn edit_request(
+    _token: ReqData<TokenClaims>,
+    edit_request_body: web::Json<EditRequestBody>,
+) -> SyphonResult<HttpResponse> {
+    let db = db();
+
+    log::info!("Edit student request: {:?}", edit_request_body);
+    let edit_request_body = edit_request_body.into_inner();
+
+    let existing_request = entities::requests::Entity::find_by_id(edit_request_body.request_id)
+        .one(db)
+        .await?
+        .ok_or(SyphonError::Json(
+            json!("request to edit cannot be found"), 
+            StatusCode::NOT_FOUND
+        ))?;
+
+    // update request 
+    let update_result = entities::requests::ActiveModel {
+        title: ActiveValue::Set(edit_request_body.title),
+        description: ActiveValue::Set(edit_request_body.description),
+        is_clusterable: ActiveValue::Set(edit_request_body.is_clusterable),
+        ..existing_request.clone().into()
+    }
+    .update(db)
+    .await?;
+
+    // delete all tags, then insert them again
+    entities::request_tags::Entity::delete_many()
+        .filter(entities::request_tags::Column::RequestId.eq(edit_request_body.request_id))
+        .exec(db)
+        .await?;
+
+    // reinsert new tags
+    let tag_insertion = edit_request_body.tags.into_iter().map(|tag_id| {
+        entities::request_tags::ActiveModel {
+            request_id: ActiveValue::Set(edit_request_body.request_id),
+            tag_id: ActiveValue::Set(tag_id),
+        }
+        .insert(db)
+    });
+
+    // handle priority tag logic
+    join_all(tag_insertion).await;
+    let is_priority = entities::tags::Entity::find()
+        .left_join(entities::queues::Entity)
+        .right_join(entities::requests::Entity)
+        .filter(entities::request_tags::Column::RequestId.eq(edit_request_body.request_id))
+        .filter(entities::queue_tags::Column::IsPriority.eq(true))
+        .filter(entities::queue_tags::Column::QueueId.eq(edit_request_body.queue_id))
+        .one(db)
+        .await?;
+    if is_priority.is_some() {
+        entities::requests::ActiveModel {
+            order: ActiveValue::Set(0),
+            ..entities::requests::ActiveModel::from(update_result.clone())
+        }
+        .update(db)
+        .await?;
+    }
+
+    Ok(HttpResponse::Ok().json("OK"))
 }
 
 pub async fn request_info_wrapper(
---
+++ frontend/pages/edit-request/[requestid]/index.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import CreateRequestCard from '../../../components/CreateRequestCard';
+import Header from '../../../components/Header';
+import styles from './EditRequest.module.css';
+import { useRouter } from 'next/router';
+
+
+const EditRequest = () => {
+  const router = useRouter();
+
+  const [requestId, setRequestId] = useState(Number.parseInt(`${router.query.requestid}`));
+  useEffect(() => {
+    setRequestId(Number.parseInt(`${router.query.requestid}`));
+  }, [router.query.requestid]);
+
+  return <>
+    <Header />
+    <div className={styles.pageContainer}>
+      <CreateRequestCard requestId={requestId} isEditMode={true} queueId={undefined} />
+    </div>  
+  </>;
+};
+
+export default EditRequest;
