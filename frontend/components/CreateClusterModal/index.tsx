4027d77f47c403ac95210c315be018ab66b171b8
hdphuong <vuonghdphuong@gmail.com>
2023-07-30 17:43:56 +1000

    create cluster modal and add requests by tags

---
+++ frontend/components/CreateClusterModal/index.tsx
@@ -0,0 +1,131 @@
+import { Button, Card, CardActionArea, Modal, Typography } from '@mui/material';
+import styles from './CreateClusterModal.module.css';
+import { useEffect, useState } from 'react';
+import { ClusterRequest, Tag, UserRequest, isCluster } from '../../types/requests';
+import { authenticatedGetFetch, authenticatedPostFetch, formatZid } from '../../utils';
+import { toast } from 'react-toastify';
+import TagsSelection from '../TagsSelection';
+import TagBox from '../TagBox';
+
+type CreateClusterModalProps = {
+  queueId: number;
+  requests: (UserRequest | ClusterRequest)[];
+};
+
+
+const CreateClusterModal = (
+  { queueId, requests }: CreateClusterModalProps
+) => {
+
+  const [open, setOpen] = useState(false);
+  const [selectedClustering, setSelectedClustering] = useState<(UserRequest)[]>([]);
+  const [clusterableUserRequests, setClusterableUserRequests] = useState<UserRequest[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  useEffect(() => {
+    const userReqs = requests.filter((r) => !isCluster(r)) as UserRequest[];
+    setClusterableUserRequests(userReqs.filter((r) => r.isClusterable));
+  }, [requests]);
+
+  useEffect(() => {
+    const getTags = async () => {
+      let res = await authenticatedGetFetch('/queue/tags', { queue_id: queueId.toString() });
+      if (res.ok) {
+        let data = await res.json();
+        setTags(data);
+      }
+    };
+    getTags();
+  }, [queueId]);
+
+  const decideBackgroundColor = (request: UserRequest | ClusterRequest) => {
+    if (selectedClustering.some((r) => isCluster(r) ? r.clusterId === request.clusterId : r.requestId === (request as UserRequest).requestId)) {
+      return 'var(--colour-seeing)';
+    } else {
+      return 'white';
+    }
+  };
+
+  const handleClusterSubmit = async () => {
+    console.log('selectedClustering', selectedClustering);
+    if (selectedClustering.length < 2) return;
+    let res = await authenticatedPostFetch('/queue/cluster/create', {
+      queue_id: queueId,
+      request_ids: selectedClustering.map((r) => !isCluster(r) && r.requestId),
+    });
+    if (!res.ok) {
+      let err = await res.json();
+      toast(err, {
+        position: 'bottom-left',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        className: styles.toast,
+      });
+    }
+    setSelectedClustering([]);
+  };
+
+  useEffect(() => {
+    setSelectedClustering(clusterableUserRequests.filter((r) => r.tags.some((t) => selectedTags.some((st) => st.name === t.name))));
+  }, [selectedTags, clusterableUserRequests]);
+
+  return (
+    <div>
+      <div className={styles.buttonContainer}>
+        <Button onClick={() => setOpen(true)} className={styles.genericButton}> Create Cluster</Button>
+      </div>
+      <Modal 
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div className={styles.container}>
+          <div className={styles.titleContainer}> 
+            <Typography variant="h4" className={styles.title}>Create Cluster</Typography>
+          </div>
+          <TagsSelection 
+            tags={tags} 
+            isCreator={false} 
+            tagSelection={selectedTags} 
+            setTagSelection={setSelectedTags} 
+          />
+          {clusterableUserRequests.length > 0 ? clusterableUserRequests.map((request, index) => (
+            <CardActionArea 
+              key={`${index}`}
+              onClick={() => {
+                if (selectedClustering.some((r) => isCluster(r) ? r.clusterId === request.clusterId : r.requestId === (request as UserRequest).requestId)) {
+                  setSelectedClustering(selectedClustering.filter((r) => isCluster(r) ? r.clusterId !== request.clusterId : r.requestId !== (request as UserRequest).requestId));
+                } else {
+                  setSelectedClustering([...selectedClustering, request]);
+                }
+              }}
+            >
+              <Card 
+                className={styles.requestCard} 
+                style={{ backgroundColor: decideBackgroundColor(request) }}
+              >
+                <TagBox text={formatZid(request.zid)} backgroundColor='var(--colour-main-purple-200)' color='var(--colour-main-purple-900)' />
+                <Typography variant="body1">{!isCluster(request) && `${request.firstName} ${request.lastName}`}</Typography>
+                <Typography variant="h6">{!isCluster(request) && request.title}</Typography>
+                {request.tags.map((tag, index) => (
+                  <TagBox key={`${index}`} text={tag.name} backgroundColor='var(--colour-main-yellow-500)' color='white' />
+                ))}
+              </Card>
+            </CardActionArea>
+          )) :
+            <Typography variant="body1">No requests to cluster</Typography>
+          }
+          {clusterableUserRequests.length > 0 && <Button onClick={handleClusterSubmit} className={styles.submitButton}>Create Cluster</Button>}
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default CreateClusterModal;
+
--- frontend/pages/active-queue/[queueid]/index.tsx
+++ frontend/pages/active-queue/[queueid]/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import { Box, Button, Card, FormControl, InputLabel, MenuItem, Modal, Select, SelectChangeEvent, Typography } from '@mui/material';
 import styles from './ActiveQueue.module.css';
 import { useRouter } from 'next/router';
 import StudentQueueRequestCard from '../../../components/StudentQueueRequestCard';
@@ -14,6 +14,7 @@ import useSound from 'use-sound';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import StudentQueueClusterRequestCard from '../../../components/StudentQueueClusterRequestCard';
+import CreateClusterModal from '../../../components/CreateClusterModal';
 
 const ActiveQueue = () => {
   const router = useRouter();
@@ -30,6 +31,7 @@ const ActiveQueue = () => {
   const [studentCount, setStudentCount] = useState(0);
   const [numReqsUntilClose, setNumReqsUntilClose] = useState(0);
   const [selectedClustering, setSelectedClustering] = useState<number[]>([]);
+  const [openClusterModal, setOpenClusterModal] = useState(false);
 
   let { lastJsonMessage } = useAuthenticatedWebSocket('ws:localhost:8000/ws/queue', {
     queryParams: {queue_id: requestData.queueId as unknown as number},
@@ -166,30 +168,7 @@ const ActiveQueue = () => {
     }
   };
 
-  const handleClusterSubmit = async () => {
-    console.log('selectedClustering', selectedClustering);
-    if (selectedClustering.length < 2) return;
-    let res = await authenticatedPostFetch('/queue/cluster/create', {
-      queue_id: Number.parseInt(`${router.query.queueid}`),
-      request_ids: selectedClustering,
-    });
-    if (!res.ok) {
-      let err = await res.json();
-      toast(err, {
-        position: 'bottom-left',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'light',
-        className: styles.toast,
-      });
-    }
-    setSelectedClustering([]);
-  };
-
+  
   const handleCopyQueueLink = () => {
     navigator.clipboard.writeText(window.location.href.replace('active-queue', 'create-request'));
   };
@@ -253,23 +232,7 @@ const ActiveQueue = () => {
               <MenuItem key={'key'} value='prevRequestCount'>{requestData.isSortedByPreviousRequestCount ? 'Unprioritise':  'Prioritise'} by number of previous requests</MenuItem>
             </Select>
           </FormControl>
-          <FormControl size='small' >
-            <InputLabel disableAnimation sx={selectedClustering.length !== 0 ? {} : { textAlign: 'center', right: '0'}} className={styles.label} id="new-cluster-select-label"> New Cluster </InputLabel>
-            <Select
-              multiple
-              value={selectedClustering}
-              labelId="new-cluster-select-label"
-              id="new-cluster-select"
-              label='New Cluster'
-              className={styles.select}
-              displayEmpty
-              onOpen={() => setSelectedClustering([])}
-              onClose={handleClusterSubmit}
-              onChange={(e) => setSelectedClustering(e.target.value as number[])}
-            >
-              {requests.filter((r) => !isCluster(r)).map((r) => (!isCluster(r) && <MenuItem key={`${r.requestId} clustering`} value={r.requestId}>{r.title}</MenuItem>))}
-            </Select>
-          </FormControl>
+          <CreateClusterModal queueId={Number.parseInt(`${router.query.queueid}`)} requests={requests}/>
           <Button className={styles.closeQueueButton} variant='contained' onClick={handleCloseQueue}>Close Queue</Button>
         </div>
         <Box className={styles.cardBox}>
