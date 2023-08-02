11b4d40e646182c640ce9382fd89175a6549c931
Aisha Nauman <aishanauman01@gmail.com>
2023-08-02 17:46:13 +1000

    making zid tagboxes redirect to user profile

--- frontend/components/TagBox/index.tsx
+++ frontend/components/TagBox/index.tsx
@@ -1,6 +1,9 @@
 import { CardActionArea, Typography } from '@mui/material';
 import styles from './TagBox.module.css';
 import type { JSX } from 'react';
+// eslint-disable-next-line no-duplicate-imports
+import React from 'react';
+import { useRouter } from 'next/router';
 
 type TagBoxProps = {
     text: string | JSX.Element,
@@ -11,11 +14,25 @@ type TagBoxProps = {
     bold?: boolean,
 }
 
-const TagBox = ({ text, backgroundColor, color, bold = true, onClick, isPriority }: TagBoxProps) => (
-  <CardActionArea onClick={onClick} className={styles.tagContainer} style={{ backgroundColor: isPriority ? '#EDB6B6' : backgroundColor }} >
+
+const TagBox = ({ text, backgroundColor, color, bold = true, onClick, isPriority }: TagBoxProps) => {
+  const router = useRouter();
+
+  const isZidFormat = () => {
+    if ( typeof(text) !== 'string') return false;
+    return text.match(/^z[0-9]{7}$/);
+  };
+
+  const userProfileOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    if ( typeof(text) !== 'string') return;
+    router.push(`/user-profile/${text.substr(1)}`);
+  };
+
+  return <CardActionArea onClick={isZidFormat() ? userProfileOnClick : onClick} className={styles.tagContainer} style={{ backgroundColor: isPriority ? '#EDB6B6' : backgroundColor }} >
     <Typography style={{ color: isPriority ? '#FFF' : color, fontWeight: bold ? 'bold' : 'normal' }} variant='body1'>{text}</Typography>
     {isPriority && '🔥' }
-  </CardActionArea>
-);
+  </CardActionArea>;
+};
 
 export default TagBox;
