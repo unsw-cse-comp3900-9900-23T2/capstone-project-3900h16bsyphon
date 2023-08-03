e3d2760bdad5e7206904f222d80438ad728c6f94
joanna209 <80564580+joanna209@users.noreply.github.com>
2023-08-03 14:28:38 +1000

    fix sort queue css (#86)
    * clean up sort queue styling
    
    * make better css
    
    * actually work

--- frontend/pages/active-queue/[queueid]/index.tsx
+++ frontend/pages/active-queue/[queueid]/index.tsx
@@ -32,6 +32,7 @@ const ActiveQueue = () => {
   const [studentCount, setStudentCount] = useState(0);
   const [numReqsUntilClose, setNumReqsUntilClose] = useState(0);
   const [noti, setNoti] = useState(false);
+  const [selection, setSelection] = useState(false);
 
   let { lastJsonMessage } = useAuthenticatedWebSocket('ws:localhost:8000/ws/queue', {
     queryParams: {queue_id: requestData.queueId as unknown as number},
@@ -239,20 +240,24 @@ const ActiveQueue = () => {
       </Card>
       <div className={styles.body}>
         <div className={styles.buttonContainer}>
-          <FormControl size='small' >
-            <InputLabel sx={{ textAlign: 'center', right: '0'}} className={styles.label} shrink={false} id="sort-queue-select-label"> Sort Queue </InputLabel>
+          <FormControl size='small'>
+            {!selection && <InputLabel sx={{ textAlign: 'center', right: '0' }} className={styles.label} shrink={false} id="sort-queue-select-label">SORT QUEUE</InputLabel>}
             <Select
               labelId="sort-queue-select-label"
               id="sort-queue-select"
               className={styles.select}
               displayEmpty
               onChange={handleSubmit}
+              sx={{
+                boxShadow: 1,
+              }}
+              onClick={() => setSelection(true)}
             >
-              {tags.map((tag) => (<MenuItem key={tag.tagId} value={tag.tagId}>{tag.isPriority ? 'Unprioritise':  'Prioritise'} &quot;{tag.name}&quot;</MenuItem>))}
-              <MenuItem key={'key'} value='prevRequestCount'>{requestData.isSortedByPreviousRequestCount ? 'Unprioritise':  'Prioritise'} by number of previous requests</MenuItem>
+              {tags.map((tag) => (<MenuItem key={tag.tagId} value={tag.tagId} sx={{ textAlign: 'center', right: '0' }}>{tag.isPriority ? 'Unprioritise':  'Prioritise'} &quot;{tag.name}&quot;</MenuItem>))}
+              <MenuItem key={'key'} value='prevRequestCount'>{requestData.isSortedByPreviousRequestCount ? 'Unprioritise':  'Prioritise'} by previous request count</MenuItem>
             </Select>
           </FormControl>
-          <CreateClusterModal queueId={Number.parseInt(`${router.query.queueid}`)} requests={requests} button={<Button  className={styles.genericButton}>Create Cluster</Button>} />
+          <CreateClusterModal queueId={Number.parseInt(`${router.query.queueid}`)} requests={requests} button={<Button className={styles.genericButton} variant="contained">Create Cluster</Button>} />
           <Button className={styles.closeQueueButton} variant='contained' onClick={handleCloseQueue}>Close Queue</Button>
         </div>
         <Box className={styles.cardBox}>
